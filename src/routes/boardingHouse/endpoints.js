750154acbae446fcf31d20c35248046d19a6227e
robert <robertcumahig@gmail.com>
2022-12-26 20:26:21 +0800

    clear

--- src/routes/boardingHouse/endpoints.js
+++ src/routes/boardingHouse/endpoints.js
@@ -33,30 +33,6 @@ internals.endpoints = [
       auth: false,
     },
   },
-  {
-    method: ["GET"],
-    path: "/offer/{id}",
-    handler: Handlers.get_offer,
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: ["POST"],
-    path: "/offer/{id}",
-    handler: Handlers.update_offer,
-    config: {
-      auth: "token",
-    },
-  },
-  {
-    method: ["GET"],
-    path: "/establishments/list", //Accommodation
-    handler: Handlers.establishment_list,
-    config: {
-      auth: "token",
-    },
-  },
 ];
 
 module.exports = internals;
